bc3386864fd83257a124ac0e2c88a348f42f3cc8
Nancy Trinh <nancytrinh20@gmail.com>
2019-12-29 19:57:42 -0800

    Adjust the page shown when the buttons are clicked and disable the paging controls to prevent a user from going outside valid bounds

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -12,6 +12,16 @@ class Table extends Component {
     };
   }
 
+  previousPage = (event) => {
+    event.preventDefault(); 
+    this.setState({page: this.state.page - 1});
+  }
+
+  nextPage = (event) => {
+    event.preventDefault(); 
+    this.setState({page: this.state.page - 1});
+  }
+
   render() {
     const headerCells = this.props.columns.map((col) => {
       return <th key={col.name}>{col.name}</th>;
@@ -40,8 +50,8 @@ class Table extends Component {
         </table>
         <div>
           <p>Showing {start} - {start + this.props.perPage} of {this.props.rows.length} total routes.</p>
-          <button>Previous Page</button>
-          <button>Next Page</button>
+          <button key="previous" disabled={this.state.page === 0} onClick={this.previousPage}>Previous Page</button>
+          <button key="next" disabled={start + this.props.perPage >= this.props.rows.length} onClick={this.nextPage}>Next Page</button>
         </div>
       </div>
     );
