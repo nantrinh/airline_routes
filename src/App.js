f870be4e64acdfc5529b522f4942508e0a2f4e70
Nancy Trinh <nancytrinh20@gmail.com>
2019-12-31 12:26:32 -0800

    Extract Select component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import DATA from './data';
 import Table from './components/Table';
+import Select from './components/Select';
 
 class App extends Component {
   formatValue(property, value) {
@@ -20,12 +21,17 @@ class App extends Component {
     ];
     
     const routes = DATA.routes; 
+    const filteredAirlines = DATA.airlines;
 
     return (
       <div className="app">
         <header className="header">
           <h1 className="title">Airline Routes</h1>
         </header>
+        <section>
+        Show routes on <Select options={filteredAirlines} valueKey="id" titleKey="name"
+  allTitle="All Airlines" value="" onSelect="" />
+        </section>
         <section>
           <Table className="routes-table" columns={columns} rows={routes} format={this.formatValue} />
         </section>
---
+++ src/components/Select.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Select extends Component {
+  render() {
+    return (
+      <select>
+        <option>All Airlines</option>
+        <option>American Airlines</option>
+        <option>Russian Airlines</option>
+        <option>United Airlines</option>
+      </select>
+    ); 
+  };
+}
+
+export default Select;
